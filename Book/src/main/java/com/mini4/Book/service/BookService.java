b48020c8807d8b79b783222659f9b1396715ad86
BoHyeon <chobh09@gmail.com>
2025-05-30 14:11:34 +0900

    도서 등록

--- Book/src/main/java/com/mini4/Book/service/BookService.java
+++ Book/src/main/java/com/mini4/Book/service/BookService.java
@@ -1,4 +1,39 @@
 package com.mini4.Book.service;
 
+import com.mini4.Book.domain.*;
+import com.mini4.Book.dto.RegisterRequestDto;
+import com.mini4.Book.exception.ResourceNotFoundException;
+import com.mini4.Book.repository.BookRepository;
+import com.mini4.Book.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class BookService {
+
+    private final BookRepository bookRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long register(RegisterRequestDto dto) {
+        User user = userRepository.findById(dto.getUserId()).orElseThrow(() -> new ResourceNotFoundException());
+
+        Book book = Book.builder()
+                .user(user)
+                .title(dto.getTitle())
+                .author(dto.getAuthor())
+                .publisher(dto.getPublisher())
+                .publishedDate(dto.getPublishedDate())
+                .content(dto.getContent())
+                .price(dto.getPrice())
+                .category(dto.getCategory())
+                .imageUrl(dto.getImageUrl())
+                .build();
+
+        return bookRepository.save(book).getId();
+    }
 }
